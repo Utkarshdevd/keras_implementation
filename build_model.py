7f396cdb2c59c2921f7b6898ffbc780cc8a47d9e
Utkarsh Dwivedi <utkarshdvd@gmail.com>
2017-09-11 09:46:26 +0530

    trying to solve timedistributed

--- build_model.py
+++ build_model.py
@@ -1,7 +1,7 @@
 from attention import SpatialAttention
 from encoder_resnet50 import ResNet50obj
 from keras.models import Model
-from keras.layers import LSTM, concatenate
+from keras.layers import LSTM, Dense, TimeDistributed, RepeatVector, Activation, Flatten
 from keras.layers.core import Lambda
 from keras import backend as K
 
@@ -11,13 +11,18 @@ def expand_dims(x):
 def expand_dims_output_shape(input_shape):
     return (input_shape[0], 1, input_shape[1])
 
+timesteps = 18
 net50 = ResNet50obj()
 resnet_model = net50.GetModel()
 V = resnet_model.layers[-1].output
-h = LSTM(2048)(V)
-print (h, V)
+V = Dense(2048)(V)
+V = Activation('relu')(V)
+print("V", V)
+h = LSTM(512, return_sequences=True)(V)
+h_allt = TimeDistributed(Dense(2048))(h)
+print (h_allt, V)
 #h_expanded = Lambda(expand_dims, expand_dims_output_shape)(h)
 #print (h, h_expanded, V)
-attn = SpatialAttention(2048)([V,h])
+attn = TimeDistributed(SpatialAttention(2048))([V,h])
 model = Model(inputs = resnet_model.input, outputs = attn)
 #model.summary()
