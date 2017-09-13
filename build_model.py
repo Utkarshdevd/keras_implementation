656540df19d917d0d3f08142da2304c3d4837a4c
Utkarsh Dwivedi <utkarshdwivedi@Shefali-2.local>
2017-09-13 21:29:36 +0530

    added reshape, coloring error code, needs ipython

--- build_model.py
+++ build_model.py
@@ -1,10 +1,17 @@
 from attention import SpatialAttention
 from encoder_resnet50 import ResNet50obj
 from keras.models import Model
-from keras.layers import LSTM, concatenate
+from keras.layers import LSTM, concatenate, RepeatVector, Reshape, TimeDistributed
 from keras.layers.core import Lambda
 from keras import backend as K
-
+try:
+    import IPython.core.ultratb
+except ImportError:
+    # No IPython. Use default exception printing.
+    pass
+else:
+    import sys
+    sys.excepthook = IPython.core.ultratb.ColorTB()
 def expand_dims(x):
     return K.expand_dims(x, 1)
 
@@ -14,7 +21,11 @@ def expand_dims_output_shape(input_shape):
 net50 = ResNet50obj()
 resnet_model = net50.GetModel()
 V = resnet_model.layers[-1].output
-h = LSTM(2048)(V)
+V = RepeatVector(18)(V)
+V = Reshape((49*2048,))(V)
+print(K.int_shape(V))
+h = TimeDistributed(LSTM(2048, return_sequences=True))(V)
+print(K.int_shape(h))
 print (h, V)
 #h_expanded = Lambda(expand_dims, expand_dims_output_shape)(h)
 #print (h, h_expanded, V)
