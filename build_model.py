3e97f6169984f89da52683374624f12650d5fcab
Utkarsh Dwivedi <utkarshdvd@gmail.com>
2017-09-24 12:15:30 +0530

    making A and A_g

--- build_model.py
+++ build_model.py
@@ -1,10 +1,12 @@
 from attention import SpatialAttention
 from encoder_resnet50 import ResNet50obj
 from keras.models import Model
-from keras.layers import LSTM, concatenate, RepeatVector, Reshape
+from keras.layers import LSTM, concatenate, RepeatVector, Reshape, Dense
+from keras.layers import TimeDistributed as KerasTD
 from timedist import TimeDistributed
 from keras.layers.core import Lambda
 from keras import backend as K
+
 try:
     import IPython.core.ultratb
 except ImportError:
@@ -22,8 +24,14 @@ def expand_dims_output_shape(input_shape):
 
 net50 = ResNet50obj()
 resnet_model = net50.GetModel()
-V = resnet_model.layers[-1].output
-
+A = resnet_model.layers[-1].output
+# TODO: a_g, use mean
+A_g = K.mean(A, axis=1)
+# TODO: v_g, relu
+V_g = Dense(2048, activation='relu')(A_g)
+print(K.int_shape(A_g), K.int_shape(V_g))
+# TODO: v_i(V), relu
+V = KerasTD(Dense(2048, activation='relu'))(A)
 V = Reshape((49*2048,))(V)
 V = RepeatVector(18)(V)
 print(K.int_shape(V))
@@ -34,4 +42,4 @@ print (h, V)
 #print (h, h_expanded, V)
 attn = TimeDistributed(SpatialAttention(2048))([V,h])
 model = Model(inputs = resnet_model.input, outputs = attn)
-model.summary()
+#model.summary()
