260156bd532fb5bb5b62c0a50c599dec95c1856a
Utkarsh Dwivedi <utkarshdvd@gmail.com>
2017-09-25 07:53:28 +0530

    Unfixed merge error

--- build_model.py
+++ build_model.py
@@ -1,11 +1,12 @@
 from attention import SpatialAttention
 from encoder_resnet50 import ResNet50obj
 from keras.models import Model
-from keras.layers import LSTM, concatenate, RepeatVector, Reshape, Dense
+from keras.layers import LSTM, RepeatVector, Reshape, Dense, Input, Embedding, Merge
 from keras.layers import TimeDistributed as KerasTD
 from timedist import TimeDistributed
 from keras.layers.core import Lambda
 from keras import backend as K
+from keras.layers.merge import Concatenate
 
 try:
     import IPython.core.ultratb
@@ -22,25 +23,39 @@ def expand_dims(x):
 def expand_dims_output_shape(input_shape):
     return (input_shape[0], 1, input_shape[1])
 
-timesteps = 18
+def matrix_mean(A):
+    return K.mean(A, axis=1, keepdims=False)
+
+def matrix_mean_outputShape(A_shape):
+    print("A_shape:{}".format(A_shape))
+    return (A_shape[0],1,A_shape[2])
+
+sentence_length = 18
+vocab_size = 10000
 net50 = ResNet50obj()
 resnet_model = net50.GetModel()
 A = resnet_model.layers[-1].output
-# TODO: a_g, use mean
-A_g = K.mean(A, axis=1)
-# TODO: v_g, relu
+A_g = Lambda(matrix_mean, output_shape=matrix_mean_outputShape)(A)
 V_g = Dense(2048, activation='relu')(A_g)
 print(K.int_shape(A_g), K.int_shape(V_g))
+V_g = RepeatVector(sentence_length)(V_g)
 # TODO: v_i(V), relu
 V = KerasTD(Dense(2048, activation='relu'))(A)
 V = Reshape((49*2048,))(V)
-V = RepeatVector(18)(V)
+V = RepeatVector(sentence_length)(V)
 print(K.int_shape(V))
-h = LSTM(2048, return_sequences=True)(V)
-print(K.int_shape(h))
+
+words = Input(shape=(sentence_length,))
+W = Embedding(output_dim=2048, input_dim=vocab_size, input_length=sentence_length)(words)
+print("W: {}".format(K.int_shape(W)))
+X = Concatenate(axis=-1)([V_g, W])
+h = LSTM(512, return_sequences=True)(X)
+h = KerasTD(Dense(2048))(h)
+print("h: {}, X: {}".format(K.int_shape(h), K.int_shape(X)))
 print (h, V)
 #h_expanded = Lambda(expand_dims, expand_dims_output_shape)(h)
 #print (h, h_expanded, V)
 attn = TimeDistributed(SpatialAttention(2048))([V,h])
-model = Model(inputs = resnet_model.input, outputs = attn)
+model = Model(inputs=resnet_model.input, outputs=attn)
 #model.summary()
+del IPython
