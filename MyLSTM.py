db62511bfa3cb0fe27afa4d4575ff62c1de57fef
Utkarsh Dwivedi <utkarshdvd@gmail.com>
2017-09-27 20:50:10 +0530

    errors

--- MyLSTM.py
+++ MyLSTM.py
@@ -1,20 +1,54 @@
 # -*- coding: utf-8 -*-
 
 from keras import backend as K
-from keras.layers import LSTM
+from keras.layers import LSTM, Input, Embedding
 
 class MyLSTM(LSTM):
-    def call(self, inputs, mask=None, training=None, initial_state=None):
-        self.cell._generate_dropout_mask(inputs, training=training)
-        self.cell._generate_recurrent_dropout_mask(inputs, training=training)
+    def call(self, x, mask=None):
+        # input shape: (nb_samples, time (padded with zeros), input_dim)
+        # note that the .build() method of subclasses MUST define
+        # self.input_spec with a complete input shape.
+        input_shape = self.input_spec[0].shape
+        if K._BACKEND == 'tensorflow':
+            if not input_shape[1]:
+                raise Exception('When using TensorFlow, you should define '
+                                'explicitly the number of timesteps of '
+                                'your sequences.\n'
+                                'If your first layer is an Embedding, '
+                                'make sure to pass it an "input_length" '
+                                'argument. Otherwise, make sure '
+                                'the first layer has '
+                                'an "input_shape" or "batch_input_shape" '
+                                'argument, including the time axis. '
+                                'Found input shape at layer ' + self.name +
+                                ': ' + str(input_shape))
+        if self.stateful:
+            initial_states = self.states
+        else:
+            initial_states = self.get_initial_states(x)
+        constants = self.get_constants(x)
+        preprocessed_input = self.preprocess_input(x)
+
+        last_output, outputs, states = K.rnn(self.step, preprocessed_input,
+                                            initial_states,
+                                            go_backwards=self.go_backwards,
+                                            mask=mask,
+                                            constants=constants,
+                                            unroll=self.unroll,
+                                            input_length=input_shape[1])
+        if self.stateful:
+            self.updates = []
+            for i in range(len(states)):
+                self.updates.append((self.states[i], states[i]))
+
+        if self.return_sequences:
+            return outputs
+        else:
+            return last_output
         self.extra_output = states
-        return super(LSTM, self).call(inputs,
-                                      mask=mask,
-                                      training=training,
-                                      initial_state=initial_state)
-I = Input(shape=(...))
-lstm = MyLSTM(20)
-output = lstm(I) # by calling, we actually call the `call()` and create `lstm.extra_output`
+main_input = Input(shape=(5,6,5))
+#x = Embedding(output_dim=512, input_dim=10000, input_length=6)(main_input)
+lstm = MyLSTM(20)(main_input)
 extra_output = lstm.extra_output # refer to the target
 
 calculate_function = K.function(inputs=[I], outputs=extra_output+[output]) # use function to calculate them **simultaneously**
